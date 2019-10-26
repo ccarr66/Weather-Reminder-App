f4392483af597b090a42b80675b8ca0876fe1fb2
Connor Carr <ccarr@cpp.edu>
2019-10-26 10:10:24 -0700

    Added Classes

--- Weather-Reminder-App/Program.cs
+++ Weather-Reminder-App/Program.cs
@@ -16,7 +16,7 @@ namespace Weather_Reminder_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
---
+++ Weather-Reminder-App/WeatherLookup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Reminder_App
+{
+    static class WeatherLookup
+    {
+    }
+}
